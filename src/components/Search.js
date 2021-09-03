22c801db7269a0c4381a47e8764354c5bdf620f2
Grayson Bloskas <graysonbloskas@Graysons-iMac-Pro.local>
2021-09-02 23:18:57 -0400

    setting up reducer

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,28 +3,43 @@ import './Search.css';
 import SearchIcon from '@material-ui/icons/Search';
 import MicIcon from '@material-ui/icons/Mic';
 import { Button } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
 
-function Search() {
+function Search({ hideButtons = false }) {
   const [input, setInput] = useState('');
+  const history = useHistory();
 
   const search = (e) => {
     e.preventDefault();
+
+    history.push('/search');
   };
   return (
-    <div className='search'>
+    <form className='search'>
       <div className='search__input'>
         <SearchIcon className='search__inputIcon' />
         <input value={input} onChange={(e) => setInput(e.target.value)} />
         <MicIcon />
       </div>
 
-      <div className='search__buttons'>
-        <Button onClick={search} variant='outlined'>
+    {!hideButtons ? (
+        <div className='search__buttons'>
+        <Button type='submit' onClick={search} variant='outlined'>
           Not Google Search
         </Button>
         <Button variant='outlined'>I'm Feeling Lucky</Button>
       </div>
-    </div>
+    ) : (
+        <div className='search__buttons'>
+        <Button type='submit' onClick={search} variant='outlined' className='buttonHidden'>
+          Not Google Search
+        </Button>
+        <Button variant='outlined' className='buttonHidden'>I'm Feeling Lucky</Button>
+      </div>
+    )}
+
+      
+    </form>
   );
 }
 
